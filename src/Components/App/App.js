3772590f87deedd884075a5e41d91f76623fc140
alphaseinor <alphaseinor@gmail.com>
2018-01-11 00:07:43 -0600

    added searchbar function, cleaned up App.js a little

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <SearchBar />
         <div className="App">
           <div className="App-playlist">
+          <SearchResults />
+          <Playlist />
           </div>
         </div>
       </div>
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,4 +1,17 @@
-<div class="SearchBar">
-  <input placeholder="Enter A Song, Album, or Artist" />
-  <a>SEARCH</a>
-</div>
+import React from 'react';
+import './SearchBar.css';
+
+
+class SearchBar extends React.Component {
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" />
+        <a>SEARCH</a>
+      </div>
+    );
+  }
+}
+
+export default Searchbar;
