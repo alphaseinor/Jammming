079588e6bbf044c9c1acad7961e7f88555b80307
alphaseinor <alphaseinor@gmail.com>
2018-01-10 23:43:28 -0600

    added Component directory structure, changed index.js to point to Component/App/App

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <div className="App-playlist">
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
